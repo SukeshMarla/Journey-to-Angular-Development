3d44a8de95b30d442c180eba0e395f6e9def84d1
DipeshShinde07 <dipesh@trainitsolutions.com>
2023-07-30 14:22:14 +0530

    upgraded version 15 chapter 11

--- Section 4/Station 8/Chapter 11/Client/src/app/Guards/auth.guard.ts
+++ Section 4/Station 8/Chapter 11/Client/src/app/Guards/auth.guard.ts
@@ -2,6 +2,7 @@ import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router }
 from '@angular/router';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from "@auth0/angular-jwt";
+import { promise } from 'protractor';
 @Injectable({providedIn:'root'})
 export class AuthGuard implements CanActivate {
   constructor(private router: Router){
@@ -18,7 +19,7 @@ export class AuthGuard implements CanActivate {
     const token = localStorage.token;
     return token!=null || token!=undefined;
   }
-  isTokenExpired(): boolean{
+  isTokenExpired(): boolean | Promise<boolean>{
     const helper = new JwtHelperService();
     return helper.isTokenExpired(localStorage.token)
   }
--- Section 4/Station 8/Chapter 11/Client/src/app/Login/login.component.ts
+++ Section 4/Station 8/Chapter 11/Client/src/app/Login/login.component.ts
@@ -1,18 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  loginForm = new FormGroup({
-    UserName: new FormControl('', [
+  loginForm = new UntypedFormGroup({
+    UserName: new UntypedFormControl('', [
       Validators.required,
       Validators.minLength(6),
     ]),
-    Password: new FormControl('', [
+    Password: new UntypedFormControl('', [
       Validators.required,
       Validators.minLength(6),
     ]),
